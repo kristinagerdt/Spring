3bd7abd21d7c1935c5e8054b13b2f3af528ea50b
kristinagerdt <gerdt.kristina@gmail.com>
2020-03-04 20:36:03 +0100

    gh-17 solution for spring-app9

--- src/main/java/com/gerdt/springcourse/TestSpring.java
+++ src/main/java/com/gerdt/springcourse/TestSpring.java
@@ -6,19 +6,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music rockMusic = context.getBean("rockMusic", RockMusic.class);
-        Music classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
-        Music rapMusic = context.getBean("rapMusic", RapMusic.class);
-
-        MusicPlayer rockMusicPlayer = new MusicPlayer(rockMusic);
-        rockMusicPlayer.playMusic();
-
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(classicalMusic);
-        classicalMusicPlayer.playMusic();
-
-        MusicPlayer rapMusicPlayer = new MusicPlayer(rapMusic);
-        rapMusicPlayer.playMusic();
-
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
