9b053c8d4557d063c11e35652562347ab48e254d
kristinagerdt <gerdt.kristina@gmail.com>
2020-03-04 17:11:12 +0100

    gh-11 solution for spring-app6

--- src/main/java/com/gerdt/springcourse/TestSpring.java
+++ src/main/java/com/gerdt/springcourse/TestSpring.java
@@ -6,9 +6,24 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(musicPlayer.getName() + ", volume:" + musicPlayer.getVolume());
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
         context.close();
     }
 }
+
+// scope="singleton"
+// true MusicPlayer@3b088d51 MusicPlayer@3b088d51 10 10
+
+// scope="prototype"
+// false MusicPlayer@3b088d51 MusicPlayer@1786dec2 10 50
