2ac3968a7fa560503cf70f10248b9303c9a942cc
kristinagerdt <gerdt.kristina@gmail.com>
2020-03-24 18:58:42 +0100

    add fixes, examples for work with Bean in SpringConfig

--- src/main/java/org/ex/spring/pets/bean/SpringConfig.java
+++ src/main/java/org/ex/spring/pets/bean/SpringConfig.java
@@ -1,5 +1,6 @@
 package org.ex.spring.pets.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -11,26 +12,43 @@ import java.util.List;
 @Configuration
 @PropertySource("classpath:pets.properties")
 public class SpringConfig {
+    // first cat
     @Bean
-    public Cat cat() {
+    public Cat catBella() {
         return new Cat();
     }
 
+    // first dog
+    @Value("Charlie")
+    private String dogName;
+
     @Bean
-    public Dog dog1() {
-        return new Dog("Charlie");
+    public Dog dogCharlie() {
+        return new Dog(dogName);
+    }
+
+    // second dog
+    @Bean
+    public String dogName() {
+        return "Max";
     }
 
     @Bean
-    public Dog dog2() {
+    public Dog dogMax() {
+        return new Dog(dogName());
+    }
+
+    // third dog
+    @Bean
+    public Dog dogJerry() {
         Dog dog = new Dog();
-        dog.setName("Max");
+        dog.setName("Jerry");
         return dog;
     }
 
     @Bean
     public List<Animal> animals() {
-        return new ArrayList<>(Arrays.asList(cat(), dog1(), dog2()));
+        return new ArrayList<>(Arrays.asList(catBella(), dogCharlie(), dogMax(), dogJerry()));
     }
 
     @Bean
